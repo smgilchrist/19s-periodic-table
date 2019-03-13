837cd5c7b7ca8bb50c0a41e523d4366c14cca8cb
Sidharath <bladefury3@gmail.com>
2019-03-13 14:40:48 -0400

    Added some font fixes

--- src/components/PeriodicTable/Filters.js
+++ src/components/PeriodicTable/Filters.js
@@ -11,9 +11,9 @@ export default class Filters extends Component {
   render() {
     return (
       <header>
-        <div class="group">
+        <div className="group">
           <h2>Group</h2>
-          <div class="group-row">
+          <div className="group-row">
             <a href="#">1</a>
             <a href="#">2</a>
             <a href="">3</a>
@@ -35,39 +35,39 @@ export default class Filters extends Component {
           </div>
         </div>
         
-        <div class="metals">
+        <div className="metals">
           <h2>Metals</h2>
           <a href="">Alkali metals</a>
           <a href="">Alkaline earth metals</a>
           <a href="">Lanthanoids</a>
         </div>
-        <div class="metals-two">
+        <div className="metals-two">
           <h2></h2>
           <a href="">Actinoids</a>
           <a href="">Transition metals</a>
           <a href="">Post-Transition metals</a>
         </div>
-        <div class="metalloids">
+        <div className="metalloids">
           <h2>METALLOIDS</h2>
         </div>
-        <div class="non-metals">
+        <div className="non-metals">
           <h2>NON-METALS</h2>
           <a href="">Other nonmetals</a>
           <a href="">Noble gases</a>
         </div>
-          <div class="other-classifications">
-          <h2>OTHER CLASSIFICATIONS</h2>
+          <div className="other-classifications">
+          <h2>OTHER classNameIFICATIONS</h2>
           <a href="">Pnictogens</a>
           <a href="">Chalcogens</a>
           <a href="">Halogens</a>
         </div>
-        <div class="properties">
+        <div className="properties">
           <h2>Properties</h2>
           <a href="">Electronegativity</a>
           <a href="">Electron Affinity</a>
           <a href="">Ionization</a>
         </div>
-        <div class="properties-2">
+        <div className="properties-2">
           <h2></h2>
           <a href="">Atomic Radius</a>
         </div>
